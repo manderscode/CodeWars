4711b9e874e1cea043968992c483728e9b750710
Resilient Coders <resilientcoders@Kerlins-MacBook-Pro.local>
2022-04-28 22:23:37 -0400

    Remove First and Last Character

---
+++ Level 8 /removeFirstAndLastChar.js
@@ -0,0 +1,9 @@
+// Instructions: It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.//
+
+function removeChar(str){
+    //You got this!
+   return str.slice(1, -1)
+   };
+   
+   // (1, -1) - the indexes are counted
+   // const stringArray = str.split('') // this method turns the string into an array
