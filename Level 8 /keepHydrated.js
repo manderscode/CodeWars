b80765f49ec7369455668f2efd35ae88a0ab2a3c
Resilient Coders <resilientcoders@Kerlins-MacBook-Pro.local>
2022-05-16 21:56:49 -0400

    Keep Hydrated L8

---
+++ Level 8 /keepHydrated.js
@@ -0,0 +1,29 @@
+// Instructions: Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+// For example:
+
+// P: return"number" which represents the litres of water that Nathan will drink
+// returning a number that is rounded to the smallest value and we can utilize the methods of Math.ceil 
+
+//function litres 
+// time passed in as a parameter and the number put in will be our argument
+// we should expect s number rounded to the smallest value 
+
+
+function litres(time) {
+    return Math.floor(time*0.5); 
+  //   let cycleTime = x; 
+  //   let water = 0.5;
+  //   return x
+  }
+  
+  // // function litres (3) { 
+  // return = 1 litre}
+  
+  
+  // // function litres (6.7) { 
+  // return = approx 3 litres}
