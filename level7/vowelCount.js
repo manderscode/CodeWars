834f1e7a52529abc03c21ad7c028e87853cb9771
Resilient Coders <resilientcoders@Kerlins-MacBook-Pro.local>
2022-05-21 23:13:27 -0400

    Vowel Count

---
+++ level7/vowelCount.js
@@ -0,0 +1,17 @@
+// Instructions: Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+    let vowelsCount = 0
+    
+    Array.from(str).forEach(i => {  
+      if ((i === 'a' || i === 'e' || i === 'i' || i === 'o' || i === 'u')) { 
+    vowelsCount++ 
+      }
+    }) 
+    return vowelsCount;
+  }
+  
