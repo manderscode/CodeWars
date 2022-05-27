80cc5539e64296c107f2c3eb04c324cb2e788f4d
Resilient Coders <resilientcoders@Kerlins-MacBook-Pro.local>
2022-05-26 22:54:58 -0400

    Methods of String Object

---
+++ Level 8 /methodsOfStringObject.js
@@ -0,0 +1,18 @@
+// Instructions: Task
+// Coding in function firstToLast, function accept 2 parameters:str and c. str is a string. c is a char.
+
+// Please return the gap between the first position of c and the last position of c.
+
+// If there are a lot of c in the str, should return a positive integer; If there is only one c in str, should return 0; If there is no c in the str, should return -1. Retrieval should not ignored the case.
+
+function firstToLast(str,c){
+    //coding here..
+    let g= parseInt(str.indexOf(c))
+    let l= parseInt(str.lastIndexOf(c))
+    if (g ===-1 || l === -1){
+      return -1
+  
+  }
+    return l - g
+    
+  }
