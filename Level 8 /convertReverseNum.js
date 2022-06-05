0adcbb157577d52fb3ff72d6fe647b28b542f132
Resilient Coders <resilientcoders@Kerlins-MacBook-Pro.local>
2022-06-04 23:59:20 -0400

    Convert number to Reversed Array of Digits

---
+++ Level 8 /convertReverseNum.js
@@ -0,0 +1,6 @@
+//Instructions: Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+
+function digitize(n) { 
+    return n.toString().split('').reverse().map(Number);
+ }
