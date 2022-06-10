b23667a298920575e15ea1e7ead8d6f1e0e39137
Resilient Coders <resilientcoders@Kerlins-MacBook-Pro.local>
2022-06-09 23:32:24 -0400

    Century From Year

---
+++ Level 8 /centuryFromYear.js
@@ -0,0 +1,10 @@
+// Instructions: Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
+
+function centuryFromYear(year) {
+    let centuryCount = 0;
+    while (year > 0){
+      year = year - 100;
+      centuryCount = centuryCount + 1;
+    }
+    return centuryCount;
+  }
